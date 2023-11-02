a3dceb1c2a8a020146145439c0f26bc1b23a99c1
Aayushi Shah <124515505+Aayushi595@users.noreply.github.com>
2023-11-02 22:39:06 +0530

    Update run.py

--- DueCheckingApplication-project_with_dummycode/run.py
+++ DueCheckingApplication-project_with_dummycode/run.py
@@ -2,7 +2,13 @@ from nodues import app, db
 
 with app.app_context():
     db.create_all()
+    
+@app.route("/")   
+   def home():
+       return render_template('index.html')
+
+
 
 if __name__ == '__main__':
-    app.run(port = 8083, debug=True )
+    app.run(debug=False, host='0.0.0.0' )
 
